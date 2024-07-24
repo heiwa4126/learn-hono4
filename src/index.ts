3992231507d5e12a4bb61717dc382866750f415f
heiwa4126 <heiwa4126@gmail.com>
2024-07-24 16:00:41 +0900

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
-import route from "./route";
+import users from "./users";
 
 const app = new OpenAPIHono();
-
-app.openapi(route, (c) => {
-	const { id } = c.req.valid("param");
-	return c.json({
-		id,
-		age: 20,
-		name: "Ultra-man",
-	});
-});
+app.route("/users", users);
 
 // The OpenAPI documentation will be available at /doc
 app.doc("/doc", {
---
+++ src/users.ts
@@ -0,0 +1,34 @@
+import { OpenAPIHono, createRoute } from "@hono/zod-openapi";
+import { ParamsSchema, UserSchema } from "./schemes";
+
+const route = createRoute({
+	method: "get",
+	path: "/{id}",
+	request: {
+		params: ParamsSchema,
+	},
+	responses: {
+		200: {
+			content: {
+				"application/json": {
+					schema: UserSchema,
+				},
+			},
+			description: "Retrieve the user",
+		},
+	},
+});
+
+const app = new OpenAPIHono();
+
+app.openapi(route, (c) => {
+	const { id } = c.req.valid("param");
+	return c.json({
+		id,
+		age: 20,
+		name: "Ultra-man",
+	});
+});
+
+export type AppType = typeof route;
+export default app;
